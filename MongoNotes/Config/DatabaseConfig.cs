66b9b08475c21b29971b0b797cd96c9affbdba80
LUIZ NUNES DE ALMEIDA JUNIOR <l.n.almeida.ti@gmail.com>
2024-03-07 13:39:46 -0300

    Completed

--- MongoNotes/Config/DatabaseConfig.cs
+++ MongoNotes/Config/DatabaseConfig.cs
@@ -9,19 +9,15 @@ public static class DatabaseConfig
         services.AddSingleton<IMongoClient>(new MongoClient(connectionStr));
 
         var client = services.BuildServiceProvider().GetRequiredService<IMongoClient>();
-
-        // Verifique a existência do banco de dados
+        
         var database = client.GetDatabase("notesData");
         if (!DatabaseExists(client, "notesData"))
         {
-            // Crie o banco de dados
             client.GetDatabase("notesData");
-
-            // Crie a coleção dentro do banco de dados
+            
             database.CreateCollection("notes");
         }
-
-        // Adicione o IMongoCollection<Note> como um serviço
+        
         services.AddSingleton<IMongoCollection<Note>>(provider =>
         {
             var client = provider.GetRequiredService<IMongoClient>();
